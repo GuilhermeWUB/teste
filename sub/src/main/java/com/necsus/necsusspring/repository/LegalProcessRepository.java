2e9af802a5c954d31178f7f8ba3290fec120a957
GuilhermeWUB <wolff.guilherme.ub@gmail.com>
2025-11-13 17:48:40 -0300

    Filter board events already sent to legal

--- sub/src/main/java/com/necsus/necsusspring/repository/LegalProcessRepository.java
+++ sub/src/main/java/com/necsus/necsusspring/repository/LegalProcessRepository.java
@@ -2,9 +2,14 @@ package com.necsus.necsusspring.repository;
 
 import com.necsus.necsusspring.model.LegalProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LegalProcessRepository extends JpaRepository<LegalProcess, Long> {
     Optional<LegalProcess> findByNumeroProcesso(String numeroProcesso);
+
+    @Query("select lp.sourceEventId from LegalProcess lp where lp.sourceEventId is not null")
+    List<Long> findAllSourceEventIds();
 }
