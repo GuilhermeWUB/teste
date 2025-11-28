8023c26a83405cd319519c80183a4831a4101a78
GuilhermeWUB <wolff.guilherme.ub@gmail.com>
2025-11-28 15:23:24 -0300

    Adjust regional API path

--- sub/src/main/java/com/necsus/necsusspring/controller/RegionalController.java
+++ sub/src/main/java/com/necsus/necsusspring/controller/RegionalController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/crm/minha-empresa/regionais")
+@RequestMapping("/crm/api/regionais")
 @PreAuthorize("hasAnyRole('ADMIN', 'DIRETORIA', 'GERENTE', 'GESTOR')")
 public class RegionalController {
 
